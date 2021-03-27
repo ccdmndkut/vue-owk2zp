391ede1e5e71d442730ff28d99456f5d1de9b94a
ccdmndkut <ccdmndkut@gmail.com>
2021-03-27 03:31:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-owk2zp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-owk2zp)
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <pre>{{ pre }}</pre>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: "App",
+  components: {},
+ async mounted() {
+await fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+  },
+  data() {
+    return {
+      imgArr: [],
+      pre: "pre"
+    };
+  }
+};
+</script>
+
+<style scoped type="scss">
+  html,
+body {
+  height: 100vh;
+  overflow: hidden;
+  max-height: 100vh;
+  margin: 0px;
+  padding: 0px;
+}
+  * {
+    margin: 0px;
+  }
+#app {
+  background: black;
+  min-height: 100%;
+
+}
+</style>
